adc13bb16ca090a990f9c038187cd0552a64f96e
Radek Burget <burgetr@fit.vutbr.cz>
2015-04-14 16:10:16 +0200

    Testing

--- src/main/java/org/fit/layout/eswc/BlockBrowserEswc.java
+++ src/main/java/org/fit/layout/eswc/BlockBrowserEswc.java
@@ -79,7 +79,7 @@ public class BlockBrowserEswc extends BlockBrowser
                     
                     /* CEUR */
                     //URL url = new URL(localpath + "/test/ceur/volumes/Vol-1317.html");
-                    URL url = new URL("http://ceur-ws.org/Vol-1333/");
+                    //URL url = new URL("http://ceur-ws.org/Vol-1333/");
                     //URL url = new URL("http://ceur-ws.org/Vol-1317/");
                     //URL url = new URL("http://ceur-ws.org/Vol-585/");
                     //URL url = new URL("http://ceur-ws.org/Vol-540/");
@@ -87,6 +87,7 @@ public class BlockBrowserEswc extends BlockBrowser
                     //URL url = new URL("http://ceur-ws.org/Vol-164/"); //(tocna)
                     //URL url = new URL("http://ceur-ws.org/Vol-104/");
                     //URL url = new URL("http://ceur-ws.org/Vol-100/");
+                    URL url = new URL("http://ceur-ws.org/Vol-53/");
                     //URL url = new URL("http://ceur-ws.org/Vol-15/"); //TODO total chaos
                     //URL url = new URL("http://ceur-ws.org/Vol-5/");
                     //URL url = new URL("http://ceur-ws.org/Vol-1/");
