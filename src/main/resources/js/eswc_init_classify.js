bd12d49ca0eb61d53122a15540f78f2ee835bd1b
Radek Burget <burgetr@fit.vutbr.cz>
2015-04-28 23:59:42 +0200

    Scripting support

--- src/main/resources/js/eswc_init_classify.js
+++ src/main/resources/js/eswc_init_classify.js
@@ -1,7 +1,9 @@
+/*
+ * Experiments with classification - not used.
+ */
 
 function processPage(url)
 {
-	//rendering
 	var srcConfig = {
 			width: 1200,
 			height: 800
@@ -9,28 +11,34 @@ function processPage(url)
 	srcConfig.url = url;
 	proc.renderPage('FitLayout.CSSBox', srcConfig);
 
-	//segmentation
 	proc.initAreaTree('FitLayout.Grouping', {});
-	//proc.apply('FitLayout.Segm.FindLines', {useConsistentStyle: false, maxLineEmSpace: 1.5});
-	//proc.apply('FitLayout.Segm.HomogeneousLeaves', {});
+	proc.apply('FitLayout.Segm.FindLines', {useConsistentStyle: false, maxLineEmSpace: 1.5});
+	proc.apply('FitLayout.Segm.HomogeneousLeaves', {});
 	//proc.apply('FitLayout.Segm.SuperAreas', {depthLimit: 2});
-	//proc.apply('Ceur.Tag.Class', {});
-	proc.apply('FitLayout.Tag.Entities', {});
-	proc.apply('Eswc.Tag.All', {});
-	
-	//logical tree
-	proc.initLogicalTree('CEUR.Logical', {});
-	
+	proc.apply('Ceur.Tag.Class', {});
+	extr.extractInstances(proc.areaTree.root);
+	println("Processed " + url + " we have " + extr.count() + " instances");
+}
+
+function train()
+{
+	proc.execInternal('js/train.js');
+}
+
+function test()
+{
+	proc.execInternal('js/test.js');
 }
 
 println("ESWC init done.");
 
 //processPage('file:/home/burgetr/git/TestingLayout/test/ceur/volumes/Vol-1317.html');
 processPage('http://ceur-ws.org/Vol-1317/');
-/*processPage('http://ceur-ws.org/Vol-1186/');
+processPage('http://ceur-ws.org/Vol-1186/');
 processPage('http://ceur-ws.org/Vol-1128/');
 processPage('http://ceur-ws.org/Vol-1123/');
 processPage('http://ceur-ws.org/Vol-1116/');
-processPage('http://ceur-ws.org/Vol-1111/');*/
+processPage('http://ceur-ws.org/Vol-1111/');
 
+extr.save('/tmp/out.arff');
 println("processing done.");
